9b5b52c9a893f9dc25764f9adccddddacfdc7366
gomerina <80198660+gomerina@users.noreply.github.com>
2021-11-22 12:53:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ function setLink () {
 	$('.main-block__text').find('.link-btn__box').appendTo('.main-block');
 }
 window.addEventListener("resize", function() {
-    if (window.innerWidth < 1024) {
-    	setLink();
-    }
-    else {
-    	$('.main-block').find('.link-btn__box').appendTo('.main-block__text');
-    }
+	if (window.innerWidth < 1024) {
+		setLink();
+	}
+	else {
+		$('.main-block').find('.link-btn__box').appendTo('.main-block__text');
+		$('.header__menu').removeClass('active');
+		$('.header__burger').removeClass('active');
+		$('html').css('overflow','hidden auto');
+	}
 });
 function res (){
 	if (window.innerWidth < 1024) {
@@ -19,3 +22,15 @@ function res (){
 	}	
 }
 res();
+
+// Открыть адаптивное меню
+$('.header__burger').click(function(){
+	$('.header__menu').toggleClass('active');
+	$('.header__burger').toggleClass('active');
+	// Блокируем прокрутку страницы при открытии меню
+	if ($('.header__menu').hasClass('active')) {
+		$('html').css('overflow','hidden')
+	} else {
+		$('html').css('overflow','hidden auto')
+	}
+});
