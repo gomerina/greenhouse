3fb5d38f8fcf93beb3a98401c6e8e8bc838aa649
gomerina <80198660+gomerina@users.noreply.github.com>
2021-11-22 11:33:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // Меняем положение кнопки в разметке на адаптиве. Главная страница первый экран
 function setLink () {
 	$('.main-block__text').find('.link-btn__box').appendTo('.main-block');
-	// $('.main-block').find('.link-btn__box').appendTo('.main-block__text');
 }
 window.addEventListener("resize", function() {
     if (window.innerWidth < 1024) {
@@ -11,3 +10,12 @@ window.addEventListener("resize", function() {
     	$('.main-block').find('.link-btn__box').appendTo('.main-block__text');
     }
 });
+function res (){
+	if (window.innerWidth < 1024) {
+		setLink();
+	}
+	else {
+		$('.main-block').find('.link-btn__box').appendTo('.main-block__text');
+	}	
+}
+res();
